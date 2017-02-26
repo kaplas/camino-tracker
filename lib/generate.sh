cc26d84102326bc4271716fd0ba4c5e257fa15a6
Jouni Kaplas <jouni.kaplas@iki.fi>
2017-02-26 22:17:27 +0200

    Add simple Here maps example code

--- lib/generate.sh
+++ lib/generate.sh
@@ -9,11 +9,19 @@ popd > /dev/null
 PARENTDIR="$(dirname "$THISDIR")"
 PUBLICDIR="$PARENTDIR/public"
 BUILDDIR="$PARENTDIR/build"
+LIBDIR="$PARENTDIR/lib"
 
 # Copy all static files from the public directory to the build directory
 rm -rf $BUILDDIR
 cp -r $PUBLICDIR $BUILDDIR
 
 # Create location file for the frontend app
-echo "window.locationEntries = " > "$BUILDDIR/js/location-data.js"
-node lib/json-content.js >> "$BUILDDIR/js/location-data.js"
+# echo "window.locationEntries = " > "$BUILDDIR/js/location-data.js"
+# node "$LIBDIR/json-content.js" >> "$BUILDDIR/js/location-data.js"
+
+# Copy test data in place instead of generating it
+cp "$PARENTDIR/test-data.js" "$BUILDDIR/js/location-data.js"
+
+# Create a secrets files
+echo "window.HERE_APP_ID = \"$HERE_APP_ID\";" > "$BUILDDIR/js/secrets.js"
+echo "window.HERE_APP_CODE = \"$HERE_APP_CODE\";" >> "$BUILDDIR/js/secrets.js"
